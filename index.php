75177d2c47cb727a3f6093b842a060f3b9461d40
higoka <nico.rickenbach@gmail.com>
2019-10-25 15:07:03 +0200

    process all swfs inside a directory

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-shell_exec("java -jar ffdec/ffdec.jar -swf2xml {$argv[1]} tmp.xml");
+function remove(string $file): void
+{
+    shell_exec("java -jar ffdec/ffdec.jar -swf2xml {$file} tmp.xml");
+    file_put_contents('tmp.xml', preg_replace('~3c(2f)?67726170686963733e~i', null, file_get_contents('tmp.xml')));
+    shell_exec("java -jar ffdec/ffdec.jar -xml2swf tmp.xml {$file}");
+}
 
-file_put_contents('tmp.xml', preg_replace('~3c(2f)?67726170686963733e~i', null, file_get_contents('tmp.xml')));
+$path = $argv[1];
 
-shell_exec("java -jar ffdec/ffdec.jar -xml2swf tmp.xml {$argv[1]}");
+if (is_dir($path) === false) {
+    remove($path);
+} else {
+    foreach (glob("{$path}/*.swf") as $file) {
+        remove($file);
+    }
+}
+
+exit("done\n");
